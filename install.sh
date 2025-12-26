7002f9b2232210b89207dd8210e4e6c56f5e3768
machine0herald <M.J.deLannoy@student.tudelft.nl>
2025-12-26 15:18:09 +0100

    generate URDF

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 
 git clone https://github.com/C-O-R-A/CoDI.git
+
 sudo apt install ros-jazzy-moveit
 sudo apt install ros-jazzy-moveit-py
 sudo rosdep-install
-pip install requirements.txt
+
+pip install -r requirements.txt
+
 sudo apt update && sudo apt upgrade
-cd .. && colcon build
+
+cd .. && colcon build --symlink-install
 
 # run with: './install.sh'
